34369f3e236472da3237faf3da03696ca1a91015
Chuyun Huang <chuyunhuang@pursuit.org>
2020-01-15 14:57:40 -0500

    fixed the decrement function by adding edge case when an item should be remove from cart completely

--- src/context.js
+++ src/context.js
@@ -101,13 +101,16 @@ class ProductProvider extends Component {
     const product = tempCart[index]
 
     product.count = product.count - 1
-    product.total = product.count * product.price
-
-    this.setState(() => {
-      return { cart: [...tempCart] }, () => {
-        this.addToCart()
-      }
-    })
+    if (product.count === 0) {
+      this.removeItem(id)
+    } else {
+      product.total = product.count * product.price
+      this.setState(() => {
+        return { cart: [...tempCart] }
+      }, () => {
+        this.addTotals()
+      })
+    }
   }
 
   //Doesn't work properly!!//
